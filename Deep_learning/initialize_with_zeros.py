db3f6472830b2cbd54bae4949823c28b85442477
hathackerwang <deanyuton@gmail.com>
2018-08-31 00:29:10 +0800

    update

---
+++ Deep_learning/initialize_with_zeros.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 11 16:13:55 2018
+
+@author: Administrator
+"""
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    w = np.zeros((dim,1))
+    b = 0
+    ### END CODE HERE ###
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
