4d18f169e299d8bf3f21f51a3a700d83fcc9f43d
hathackerwang <deanyuton@gmail.com>
2018-09-01 12:08:56 +0800

    update

---
+++ Deep_learning/Predict.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 11 16:14:49 2018
+
+@author: Administrator
+"""
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    ### START CODE HERE ### (≈ 1 line of code)
+    A = sigmoid(np.dot(X.T, w) + b).T
+    ### END CODE HERE ###
+    
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        ### START CODE HERE ### (≈ 4 lines of code)
+        if( A[0, i] <= 0.5):
+            Y_prediction[0,i] = 0
+        else:
+            Y_prediction[0,i] = 1
+        ### END CODE HERE ###
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
